864adf80fde8bd79cd036a954e79191c54975f15
joshish <joshpersonguy@outlook.com>
2022-05-10 10:53:51 -0500

    Fixed fail string, added ping command, added latency for regex command

--- bot.py
+++ bot.py
@@ -22,6 +22,12 @@ class MyClient(commands.Bot):
 client = MyClient(command_prefix=".")
 # client=commands.Bot(command_prefix=".")
 
+@client.slash_command(name="ping", description="Ping the bot", guild_ids=[971807147627282482])
+async def ping(interaction):
+    await interaction.response.defer()
+    embed = discord.Embed(color=0xff9300)
+    embed.add_field(name="Regex crashed", value=f"Pong! {client.latency*1000}ms", inline=False)
+    await interaction.followup.send(embed=embed)
 
 @client.slash_command(name="regex", description="Parse a regex", guild_ids=[971807147627282482])
 async def about(interaction,
@@ -42,9 +48,6 @@ async def about(interaction,
         url="https://cdn.discordapp.com/attachments/887748266761007125/971808149109633094/unknown.png")
     # embed.add_field(name="String Fail Check", value="Success!", inline=True)
 
-    fname = "pics/testing"
-    path2fname = "pics/testing.gv.png"
-
     # Pass the regex
     regex_compiled = False
     try:
@@ -52,22 +55,24 @@ async def about(interaction,
         regex_to_test = Regex(expression)
         regex_match = Compiler(regex_to_test.postfix)
         regex_compiled = True
-        if flatten:
-            regex_match.transition_table()
-            regex_match.flatten()
-        regex_match.draw_transition_table(fname, format="png")
-        regex_match.transition_table()
-    except Exception as e :
-        print(e)
+    except:
         regex_compiled = False
 
     # fname = "".join([c for c in random.shuffle(ascii_letters)])
     # temp_regex_pic = tempfile.NamedTemporaryFile(suffix='.png')
 
+    fname = "pics/testing"
+    path2fname = "pics/testing.gv.png"
 
- 
+    if flatten:
+        regex_match.transition_table()
+        regex_match.flatten()
+    regex_match.draw_transition_table(fname, format="png")
+    regex_match.transition_table()
 
     if regex_compiled:
+
+        embed.add_field(name="Regex compiled successfully!", value=f"The regex was compiled in {client.latency*1000}ms", inline=False)
 
         # Process the passed string
         if regex_match.automata.match(success):
@@ -81,7 +86,7 @@ async def about(interaction,
         if fail != None:
             if not regex_match.automata.match(fail):
                 embed.add_field(name="String Reject Check Success",
-                                value=f"The regex {expression} passed the test of rejcting {success}", inline=False)
+                                value=f"The regex {expression} passed the test of rejecting {fail}", inline=False)
 
         file2disc = discord.File(path2fname)
         embed.set_image(url=f"attachment://{path2fname}")
@@ -90,7 +95,7 @@ async def about(interaction,
 
     else:
         embed.add_field(name="Regex crashed",
-                        value=f"The regex {expression} crashed the program", inline=False)
+                        value=f"The regex {expression} crashed the program in {client.latency*1000}ms", inline=False)
         await interaction.followup.send(embed=embed)
 
 token = ""
